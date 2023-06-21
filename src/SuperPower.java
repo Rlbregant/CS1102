3f5517749ff91ecc7c953c3e143df3e5ac21453a
RossB <23426097+Rlbregant@users.noreply.github.com>
2023-06-21 14:49:08 -0700

    Finished Assignment

---
+++ src/SuperPower.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class SuperPower {
+    public static void main(String[] args) {
+        String power = JOptionPane.showInputDialog("What is your super power?");
+                JOptionPane.showMessageDialog(null,power = power.toUpperCase()+" TO THE RESCUE!");
+    }
+}
